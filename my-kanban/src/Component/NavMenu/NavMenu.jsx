896167986ac13a8127990fe3df4a66ca6235efe3
Mahmud Hasan Imran <mahmud.bubt.150@gmail.com>
2022-11-16 01:14:34 +0600

    Added Navbar

--- my-kanban/src/Component/NavMenu/NavMenu.jsx
+++ my-kanban/src/Component/NavMenu/NavMenu.jsx
@@ -8,12 +8,17 @@ const NavMenu = () => {
                     <h1>Kanban</h1>
                 </div>
                 <div className="menu__items">
-                    <ul className='lg:flex'>
-                        <li className='mx-6'>Home</li>
-                        <li className='mx-6'>About Us</li>
-                        <li className='mx-6'>Pricing</li>
-                        <li className='mx-6'>Resources</li>
-                        <li className='mx-6'>Contact</li>
+                    <ul className='lg:flex font-semibold items-center'>
+                        <li className='mx-5'>Home</li>
+                        <li className='mx-5'>About Us</li>
+                        <li className='mx-5'>Pricing</li>
+                        <li className='mx-5'>Resources</li>
+                        <li className='mx-5'>Contact</li>
+                        <li>
+                            <button className='login__btn bg-[#197AFF] text-white px-6 py-2.5 mx-2 rounded-3xl'>Login</button>
+                            <button className='login__btn border border-dotted border-[#197AFF] text-[#197AFF] px-6 py-2.5 mx-2 rounded-3xl'>Register</button>
+                            
+                        </li>
                     </ul>
                 </div>
             </div>
